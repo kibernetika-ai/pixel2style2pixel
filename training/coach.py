82c1e3a75d3a87f6ac0234753ba189a62cbe06f0
Alex <agunin@kibernetika.ai>
2020-11-07 19:54:38 -0800

    try mix

--- training/coach.py
+++ training/coach.py
@@ -151,7 +151,7 @@ class Coach:
 				f.write('Step - {}, \n{}\n'.format(self.global_step, loss_dict))
 
 	def configure_optimizers(self):
-		params = list(self.net.encoder1.parameters())
+		params = list(self.net.encoder.parameters())
 		params += list(self.net.encoder2.parameters())
 		if self.opts.train_decoder:
 			params += list(self.net.decoder.parameters())
