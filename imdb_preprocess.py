6d900437ef574b6e13c2cb3da60c7f1b43ed9021
Nikolay Mahotkin <nikolay.makhotkin@gmail.com>
2020-12-23 15:56:49 +0400

    Add imdb preprocess by landmarks

---
+++ imdb_preprocess.py
@@ -0,0 +1,181 @@
+import argparse
+import glob
+import math
+import os
+import pickle
+import sys
+
+import cv2
+import face_alignment
+import numpy as np
+import torch
+
+from transform3d import euler
+
+
+face_model_path = (
+    '/opt/intel/openvino/deployment_tools/intel_models'
+    '/face-detection-adas-0001/FP32/face-detection-adas-0001.xml'
+)
+
+
+def best_fit_transform(A, B):
+    '''
+    Calculates the least-squares best-fit transform that maps corresponding points A to B in m spatial dimensions
+    Input:
+      A: Nxm numpy array of corresponding points
+      B: Nxm numpy array of corresponding points
+    Returns:
+      T: (m+1)x(m+1) homogeneous transformation matrix that maps A on to B
+      R: mxm rotation matrix
+      t: mx1 translation vector
+    '''
+
+    assert A.shape == B.shape
+
+    # get number of dimensions
+    m = A.shape[1]
+
+    # translate points to their centroids
+    centroid_A = np.mean(A, axis=0)
+    centroid_B = np.mean(B, axis=0)
+    AA = A - centroid_A
+    BB = B - centroid_B
+
+    # rotation matrix
+    H = np.dot(AA.T, BB)
+    U, S, Vt = np.linalg.svd(H)
+    R = np.dot(Vt.T, U.T)
+
+    # special reflection case
+    if np.linalg.det(R) < 0:
+       Vt[m-1,:] *= -1
+       R = np.dot(Vt.T, U.T)
+
+    # translation
+    t = centroid_B.T - np.dot(R,centroid_A.T)
+
+    # homogeneous transformation
+    T = np.identity(m+1)
+    T[:m, :m] = R
+    T[:m, m] = t
+
+    return T, R, t
+
+
+def norm3d_t(landmark, ref):
+    t, _, _ = best_fit_transform(landmark, ref)
+    #print(t)
+    n = np.dot(t[0:3, 0:3], landmark.T).T
+    n += t[:3, 3]
+    return n.astype(np.float32), t
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data-dir', required=True)
+    parser.add_argument('--limit', type=int, default=0)
+    parser.add_argument('--output-dir', required=True)
+    parser.add_argument('--threshold', default=0.5, type=float)
+    parser.add_argument('--min-size', default=100, type=int)
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    min_face_size = args.min_size
+    min_box_diagonal = int(math.sqrt(2 * (min_face_size ** 2)))
+    print_fun('List files...')
+    image_paths = glob.glob(os.path.join(args.data_dir, '**/*.jpg'))
+    print_fun(f'Done list files: {len(image_paths)}')
+
+    print_fun('Load face detect driver...')
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    fa3d = face_alignment.FaceAlignment(face_alignment.LandmarksType._3D, device=device)
+    print_fun('Done loading.')
+
+    landmark_ref = np.load('./landmark_ref.npy')
+    processed = 0
+    threshold = 0.2
+
+    landmarks = {}
+    boxes = {}
+
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+
+    for i, path in enumerate(image_paths):
+        if i % 100 == 0:
+            print_fun(f'Progress {i / len(image_paths) * 100:.2f} %.')
+            print_fun(f'Processed {processed} images, looked: {i}.')
+
+        with open(path, 'rb') as f:
+            raw_img = f.read()
+        frame = cv2.imdecode(np.frombuffer(raw_img, np.uint8), cv2.IMREAD_COLOR)
+        detected_faces = fa3d.face_detector.detect_from_image(frame[:, :, ::-1])
+        if len(detected_faces) != 1:
+            continue
+        box = detected_faces[0]
+        x1 = box[0] / frame.shape[1]
+        x2 = box[2] / frame.shape[1]
+        y1 = box[1] / frame.shape[0]
+        y2 = box[3] / frame.shape[0]
+        l3d = fa3d.get_landmarks(frame, detected_faces=[box])
+        scale = (box[2] - box[0] + box[3] - box[1]) / 195
+        if l3d is None or len(l3d) < 1:
+            continue
+        l3d = l3d[0]
+        landmark_3d = l3d[:, :].astype(np.float32)
+        landmark_3d[:, 0] = (landmark_3d[:, 0]) / frame.shape[1]
+        landmark_3d[:, 1] = (landmark_3d[:, 1]) / frame.shape[0]
+        landmark_3d[:, 2] = landmark_3d[:, 2] / (200 * scale)
+        landmark_3d[:, 0:2] = np.clip(landmark_3d[:, 0:2], 0, 1)
+        _, land_transform = norm3d_t(landmark_3d.copy(), landmark_ref)
+
+        a1, a2, a3 = euler.mat2euler(land_transform)
+
+        # print(a1, a2, a3)
+        # cv2.imshow('Image', frame)
+        # cv2.waitKey(0)
+
+        is_frontal = abs(a1) < threshold and abs(a2) < threshold and abs(a3) < threshold
+        if not is_frontal:
+            continue
+
+        dirname = path.split('/')[-2]
+        basename = os.path.basename(path)
+        save_path = os.path.join(args.output_dir, dirname, basename)
+        if not os.path.exists(os.path.join(args.output_dir, dirname)):
+            os.makedirs(os.path.join(args.output_dir, dirname))
+        with open(save_path, 'wb') as f:
+            f.write(raw_img)
+
+        landmarks[f'{dirname}/{basename}'] = landmark_3d
+        boxes[f'{dirname}/{basename}'] = np.array([x1, y1, x2, y2])
+        processed += 1
+
+        if args.limit != 0 and processed >= args.limit:
+            break
+
+    with open(os.path.join(args.output_dir, 'landmarks.pkl'), 'wb') as f:
+        pickle.dump(landmarks, f)
+    with open(os.path.join(args.output_dir, 'boxes.pkl'), 'wb') as f:
+        pickle.dump(boxes, f)
+
+    print_fun(f'Processed {processed} images, looked: {i}')
+
+
+def print_fun(s):
+    print(s)
+    sys.stdout.flush()
+
+
+def box_diagonal(box):
+    w = box[2] - box[0]
+    h = box[3] - box[1]
+    return math.sqrt(w ** 2 + h ** 2)
+
+
+if __name__ == '__main__':
+    main()
---
+++ transform3d/euler.py
@@ -0,0 +1,155 @@
+import numpy as np
+import math
+# axis sequences for Euler angles
+_NEXT_AXIS = [1, 2, 0, 1]
+
+# map axes strings to/from tuples of inner axis, parity, repetition, frame
+_AXES2TUPLE = {
+    'sxyz': (0, 0, 0, 0), 'sxyx': (0, 0, 1, 0), 'sxzy': (0, 1, 0, 0),
+    'sxzx': (0, 1, 1, 0), 'syzx': (1, 0, 0, 0), 'syzy': (1, 0, 1, 0),
+    'syxz': (1, 1, 0, 0), 'syxy': (1, 1, 1, 0), 'szxy': (2, 0, 0, 0),
+    'szxz': (2, 0, 1, 0), 'szyx': (2, 1, 0, 0), 'szyz': (2, 1, 1, 0),
+    'rzyx': (0, 0, 0, 1), 'rxyx': (0, 0, 1, 1), 'ryzx': (0, 1, 0, 1),
+    'rxzx': (0, 1, 1, 1), 'rxzy': (1, 0, 0, 1), 'ryzy': (1, 0, 1, 1),
+    'rzxy': (1, 1, 0, 1), 'ryxy': (1, 1, 1, 1), 'ryxz': (2, 0, 0, 1),
+    'rzxz': (2, 0, 1, 1), 'rxyz': (2, 1, 0, 1), 'rzyz': (2, 1, 1, 1)}
+
+_TUPLE2AXES = dict((v, k) for k, v in _AXES2TUPLE.items())
+
+# For testing whether a number is close to zero
+_EPS4 = np.finfo(float).eps * 4.0
+
+
+def euler2mat(ai, aj, ak, axes='sxyz'):
+    """Return rotation matrix from Euler angles and axis sequence.
+    Parameters
+    ----------
+    ai : float
+        First rotation angle (according to `axes`).
+    aj : float
+        Second rotation angle (according to `axes`).
+    ak : float
+        Third rotation angle (according to `axes`).
+    axes : str, optional
+        Axis specification; one of 24 axis sequences as string or encoded
+        tuple - e.g. ``sxyz`` (the default).
+    Returns
+    -------
+    mat : array (3, 3)
+        Rotation matrix or affine.
+    Examples
+    --------
+    >>> R = euler2mat(1, 2, 3, 'syxz')
+    >>> np.allclose(np.sum(R[0]), -1.34786452)
+    True
+    >>> R = euler2mat(1, 2, 3, (0, 1, 0, 1))
+    >>> np.allclose(np.sum(R[0]), -0.383436184)
+    True
+    """
+    try:
+        firstaxis, parity, repetition, frame = _AXES2TUPLE[axes]
+    except (AttributeError, KeyError):
+        _TUPLE2AXES[axes]  # validation
+        firstaxis, parity, repetition, frame = axes
+
+    i = firstaxis
+    j = _NEXT_AXIS[i+parity]
+    k = _NEXT_AXIS[i-parity+1]
+
+    if frame:
+        ai, ak = ak, ai
+    if parity:
+        ai, aj, ak = -ai, -aj, -ak
+
+    si, sj, sk = math.sin(ai), math.sin(aj), math.sin(ak)
+    ci, cj, ck = math.cos(ai), math.cos(aj), math.cos(ak)
+    cc, cs = ci*ck, ci*sk
+    sc, ss = si*ck, si*sk
+
+    M = np.eye(3)
+    if repetition:
+        M[i, i] = cj
+        M[i, j] = sj*si
+        M[i, k] = sj*ci
+        M[j, i] = sj*sk
+        M[j, j] = -cj*ss+cc
+        M[j, k] = -cj*cs-sc
+        M[k, i] = -sj*ck
+        M[k, j] = cj*sc+cs
+        M[k, k] = cj*cc-ss
+    else:
+        M[i, i] = cj*ck
+        M[i, j] = sj*sc-cs
+        M[i, k] = sj*cc+ss
+        M[j, i] = cj*sk
+        M[j, j] = sj*ss+cc
+        M[j, k] = sj*cs-sc
+        M[k, i] = -sj
+        M[k, j] = cj*si
+        M[k, k] = cj*ci
+    return M
+
+
+def mat2euler(mat, axes='sxyz'):
+    """Return Euler angles from rotation matrix for specified axis sequence.
+    Note that many Euler angle triplets can describe one matrix.
+    Parameters
+    ----------
+    mat : array-like shape (3, 3) or (4, 4)
+        Rotation matrix or affine.
+    axes : str, optional
+        Axis specification; one of 24 axis sequences as string or encoded
+        tuple - e.g. ``sxyz`` (the default).
+    Returns
+    -------
+    ai : float
+        First rotation angle (according to `axes`).
+    aj : float
+        Second rotation angle (according to `axes`).
+    ak : float
+        Third rotation angle (according to `axes`).
+    Examples
+    --------
+    >>> R0 = euler2mat(1, 2, 3, 'syxz')
+    >>> al, be, ga = mat2euler(R0, 'syxz')
+    >>> R1 = euler2mat(al, be, ga, 'syxz')
+    >>> np.allclose(R0, R1)
+    True
+    """
+    try:
+        firstaxis, parity, repetition, frame = _AXES2TUPLE[axes.lower()]
+    except (AttributeError, KeyError):
+        _TUPLE2AXES[axes]  # validation
+        firstaxis, parity, repetition, frame = axes
+
+    i = firstaxis
+    j = _NEXT_AXIS[i+parity]
+    k = _NEXT_AXIS[i-parity+1]
+
+    M = np.array(mat, dtype=np.float64, copy=False)[:3, :3]
+    if repetition:
+        sy = math.sqrt(M[i, j]*M[i, j] + M[i, k]*M[i, k])
+        if sy > _EPS4:
+            ax = math.atan2( M[i, j],  M[i, k])
+            ay = math.atan2( sy,       M[i, i])
+            az = math.atan2( M[j, i], -M[k, i])
+        else:
+            ax = math.atan2(-M[j, k],  M[j, j])
+            ay = math.atan2( sy,       M[i, i])
+            az = 0.0
+    else:
+        cy = math.sqrt(M[i, i]*M[i, i] + M[j, i]*M[j, i])
+        if cy > _EPS4:
+            ax = math.atan2( M[k, j],  M[k, k])
+            ay = math.atan2(-M[k, i],  cy)
+            az = math.atan2( M[j, i],  M[i, i])
+        else:
+            ax = math.atan2(-M[j, k],  M[j, j])
+            ay = math.atan2(-M[k, i],  cy)
+            az = 0.0
+
+    if parity:
+        ax, ay, az = -ax, -ay, -az
+    if frame:
+        ax, az = az, ax
+    return ax, ay, az
